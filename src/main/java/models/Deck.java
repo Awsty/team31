5ea13d6f381899861e3c3afea370c02747ab478c
danyollee <leed3@oregonstate.edu>
2017-11-12 00:00:00 -0800

    added the deckEmpty() function

--- src/main/java/models/Deck.java
+++ src/main/java/models/Deck.java
@@ -13,10 +13,10 @@ public class Deck {
     public java.util.List<Card> gameDeck = new ArrayList<Card>(52);
 
 /*    Adding new cards into the deck. We are just adding the values of the cards and since Aces are considered high,
-      we will start with 2. Aces will be value '14'.
+      we will start with 2. Aces will be value '15'.
  */
     public Deck(){
-      for(int i=2; i<=14; i++){
+      for(int i=2; i<=15; i++){
         gameDeck.add(new Card(i, Suit.Hearts));
         gameDeck.add(new Card(i, Suit.Diamonds));
         gameDeck.add(new Card(i, Suit.Spades));
@@ -24,11 +24,20 @@ public class Deck {
       }
     }
 
-    void shuffle(){
+    public void shuffle(){
+//    Collections is a utility class in java that allows us to re-aarange the array in a random order
       Collections.shuffle(gameDeck);
     }
 
-    int getRemainingAmountOfCards(){
-
+/*  This function will be called after every deal. This will check to see if the deck has been played its last card.
+    If so, that means that the user has won the game.
+*/
+    public boolean deckEmpty(){
+        if(gameDeck.size()<=0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
