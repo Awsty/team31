4450f1beaa8e500e634295368778017fd96b10ce
jawhnypoh <po.johnny@gmail.com>
2017-11-21 01:40:40 -0800

    Updating and fixing stuff

--- src/main/java/models/Deck.java
+++ src/main/java/models/Deck.java
@@ -1,43 +1,35 @@
 package models;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
-// Created by Daniel Lee
 public class Deck {
 
-  //  Creates an array of size 52 of the Card object
-    public java.util.List<Card> gameDeck = new ArrayList<Card>(52);
+    public java.util.List<Card> cards = new ArrayList<>();
 
-/*    Adding new cards into the deck. We are just adding the values of the cards and since Aces are considered high,
-      we will start with 2. Aces will be value '15'.
- */
-    public Deck(){
-      for(int i=2; i<=15; i++){
-        gameDeck.add(new Card(i, Suit.Hearts));
-        gameDeck.add(new Card(i, Suit.Diamonds));
-        gameDeck.add(new Card(i, Suit.Spades));
-        gameDeck.add(new Card(i, Suit.Clubs));
-      }
+    public Deck() {
+        for(int i = 2; i < 15; i++){
+            cards.add(new Card(i,Suit.Clubs));
+            cards.add(new Card(i,Suit.Hearts));
+            cards.add(new Card(i,Suit.Diamonds));
+            cards.add(new Card(i,Suit.Spades));
+        }
     }
 
-    public void shuffle(){
-//    Collections is a utility class in java that allows us to re-aarange the array in a random order
-      Collections.shuffle(gameDeck);
+    public void shuffle() {
+        long seed = System.nanoTime();
+        Collections.shuffle(cards, new Random(seed));
     }
 
-/*  This function will be called after every deal. This will check to see if the deck has been played its last card.
-    If so, that means that the user has won the game.
-*/
-    public boolean deckEmpty(){
-        if(gameDeck.size()<=0){
-            return true;
+    public ArrayList<Card> dealFour() {
+        ArrayList<Card> four = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            if (cards.size() > 0) {
+                four.add(cards.get(i));
+                cards.remove(i);
+            }
         }
-        else{
-            return false;
-        }
+        return four;
     }
 }
