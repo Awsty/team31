371b5e2e00370adbd2319c50b408c9164ae136d7
Brandon Mei <meib@flip3.engr.oregonstate.edu>
2017-12-03 23:17:43 -0800

    completed testDeck and testGame

---
+++ src/test/java/models/testColumn.java
@@ -0,0 +1,19 @@
+package java.models;
+
+import models.Card;
+import models.Column;
+import models.Deck;
+import models.Game;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class testColumn {
+
+
+
+}
