798247c462d9bfc8e17f4f0e38c9b1667fee1c52
Awsty <lianga@oregonstate.edu>
2017-12-03 23:49:49 -0800

    update

--- src/test/java/models/testColumn.java
+++ src/test/java/models/testColumn.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class testColumn {
     @Test
     public void testColumns(){
-	int id=0;
+	int id=0, i=0,j =0;
         SpanishDeck sd = new SpanishDeck();
         assertEquals(50,sd.cards.size());
     }
